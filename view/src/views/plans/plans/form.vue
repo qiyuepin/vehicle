bebc59ef7da3f23504a93e0716476ee15c529a8e
Bao <baolei219@163.com>
2024-06-13 12:53:12 +0800

    改修 No.10 2024/06/13 by baolei
    两条以上相同的运输计划不能更新。
    
    Co-Authored-By: qiyuepin <38338000+qiyuepin@users.noreply.github.com>

--- view/src/views/plans/plans/form.vue
+++ view/src/views/plans/plans/form.vue
@@ -111,229 +111,260 @@
 
 <script>
 
-import { addplan, editplan, getplansinfo, getplaninfo } from '@/api/plan.js'
+import { addplan, editplan, getplansinfo, getplaninfo, getPlanCount } from '@/api/plan.js'
 import UploadImage from '@/components/Upload/SingleImage'
 import { validPhone,validIDcard } from '@/utils/validate'
 
 
 
 export default {
-name: "myForm",
-components: {
-  UploadImage,
-  Map
-},
-data() {
-
-  const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('用户名称长度2-10位'))
-      } else {
-        callback()
-      }
-    }
-
-  return {
-    title:'',
-    dialog: false,
-    roles: [],
-    infolist: [],
-    factorylist: [],
-    load_factory: null,
-    load_address:'',
-    drawerShow:false,
-    map: null,
-    saveRules: {
-      product_name: [{ required: true, message: '货品名称不能为空',trigger: 'blur'}],
-      product_quantity: [{ required: true, message: '货品数量不能为空',trigger: 'blur'}],
-      plan_type: [{ required: true, message: '请选择任务类别', trigger: 'change' }],
-      // load_factory: [{ required: true, message: '装货厂家不能为空', trigger: 'blur'}],
-      // unload_factory: [{ required: true, message: '卸货厂家不能为空',trigger: 'blur'}],
+    name: "myForm",
+    components: {
+        UploadImage,
+        Map
     },
-    formData: {
-      id: 0,
-      info_id: '',
-      product_name: '',
-      product_quantity: '',
-      load_factory: '',
-      load_address: '',
-      unload_factory: '',
-      unload_address: '',
-      head_num: '',
-      trailer_num: '',
-      driver_name: '',
-      escort_name: '',
-      plan_type: '',
-      start_periodic: '',
-      end_periodic: '',
-      platform: 'pc',
-      load_location: '',
-      unload_location: ''
-    },
-  }
-},
-created() {
-  this.getplaninfo()
-},
-destroyed () {
-    if (this.map != null) {
-      this.map.destroy();
-    }
-  },
-methods: {
-  handlePlanTypeChange(value) {
-    console.log(value)
-    if (value === 0) {
-      // 如果选择了运输任务，则设置新计费周期为必填
-      this.$set(this.saveRules, 'start_periodic', [{ required: true, message: '请填写新计费周期', trigger: 'blur' }]);
-    } else {
-      // 如果选择了装车任务或者卸车任务，则取消新计费周期的必填状态
-      this.$delete(this.saveRules, 'start_periodic');
-      if (this.$refs.formData) {
-        this.$nextTick(() => {
-          this.$refs.formData.resetFields(['start_periodic']); // 清空已填写的值
-        });
-      }
-      this.formData.start_periodic = ''; // 清空已填写的值
-    }
-  },
-  getplaninfo() {
-    getplaninfo().then(response => {
-        if(response !== undefined){
-          // console.log(response.data)
-          this.infolist = response.data
-          this.factorylist = response.factory
-        }
-    })
-  },
-  infoChanged() {
-    const selectedinfo = this.infolist.find(item => item.id === this.formData.info_id);
-    if (selectedinfo) {
-      console.log(selectedinfo)
-      // this.formData.head_num = selectedinfo.head_num;
-      this.formData.trailer_num = selectedinfo.trailer_num;
-      this.formData.driver_name = selectedinfo.driver_name;
-      this.formData.trailer_status = selectedinfo.trailer_status;
-    } else {
-      // this.formData.head_num = '';
-      this.formData.trailer_num = '';
-      this.formData.driver_name = '';
-      this.formData.trailer_status = '';
-    }
-    // this.load_address = this.load_factory.factory;
-  },
-  loadFactoryChanged() {
-    const selectedFactory = this.factorylist.find(item => item.name === this.formData.load_factory);
-    if (selectedFactory) {
-      console.log(selectedFactory)
-      this.formData.load_address = selectedFactory.address;
-      this.formData.load_location = selectedFactory.location;
-    } else {
-      this.formData.load_address = '';
-      this.formData.load_location = '';
-    }
-    // this.load_address = this.load_factory.factory;
-  },
-  unloadFactoryChanged() {
-    const selectedFactory = this.factorylist.find(item => item.name === this.formData.unload_factory);
-    if (selectedFactory) {
-      console.log(selectedFactory)
-      this.formData.unload_address = selectedFactory.address;
-      this.formData.unload_location = selectedFactory.location;
-    } else {
-      this.formData.unload_address = '';
-      this.formData.unload_location = '';
-    }
-    // this.load_address = this.load_factory.factory;
-  },
-  handleClose() {
-    this.dialog = false
-    this.drawerShow = false
-  },
-  showForm() {
-    this.dialog = true
-    this.drawerShow = true
-    this.title = '新增任务'
-    this.resetData()
-  },
-  resetData(){
-    this.formData.id = 0
-    this.formData.info_id = ''
-    this.formData.product_name = ''
-    this.formData.product_quantity = ''
-    this.formData.load_address = ''
-    this.formData.load_factory = ''
-    this.formData.unload_address = ''
-    this.formData.unload_factory = ''
-    this.formData.head_num = ''
-    this.formData.trailer_num = ''
-    this.formData.driver_name = ''
-    this.formData.plan_type = ''
-    this.formData.start_periodic = ''
-    this.formData.end_periodic = ''
-    this.formData.load_location = ''
-    this.formData.unload_location = ''
-    this.formData.trailer_status = ''
-  },
-  getplansinfo(id){
-    getplansinfo({id:id}).then(response=>{
-        if(response !== undefined){
-          console.log('response.start_periodic---'+response.start_periodic)
-            this.title = '编辑'
-            this.formData.id = response.id
-            this.formData.info_id = response.info_id
-            this.formData.product_name = response.product_name
-            this.formData.product_quantity = response.product_quantity
-            this.formData.load_factory = response.load_factory
-            this.formData.load_address = response.load_address
-            this.formData.unload_address = response.unload_address
-            this.formData.unload_factory = response.unload_factory
-            this.formData.head_num = response.head_num
-            this.formData.trailer_num = response.trailer_num
-            this.formData.driver_name = response.driver_name
-            this.formData.plan_type = response.plan_type
-            this.formData.load_location = response.load_location
-            this.formData.unload_location = response.unload_location
-            this.formData.start_periodic = response.start_periodic
-            this.formData.end_periodic = response.end_periodic
-            this.formData.trailer_status = response.trailer_status
-        }
-    })
-  },
-  saveData() {
-    console.log(this.formData)
-    this.$confirm('您确定要提交吗？', '温馨提示')
-      .then(_ => {
-        this.$refs.saveForm.validate(valid => {
-          if (valid) {
-            if(this.formData.id){
-              editplan(this.formData).then(_ => {
-                this.$message({
-                  message: '编辑成功',
-                  type: 'success',
-                  duration: 5 * 1000
-                })
-                this.$emit('updateRow')
-                this.dialog = false
-              })
-            }else{
-              addplan(this.formData).then(_ => {
-                this.$message({
-                  message: '新增成功',
-                  type: 'success',
-                  duration: 5 * 1000
-                })
-                this.$emit('updateRow')
-                this.dialog = false
-              })
+    data() {
+
+        const validateUsername = (rule, value, callback) => {
+            if (!validUsername(value)) {
+                callback(new Error('用户名称长度2-10位'))
+            } else {
+                callback()
             }
-          }
-        })
-      })
-      .catch(_ => {
-      })
-  }
-}
+        }
+
+        return {
+            title: '',
+            dialog: false,
+            roles: [],
+            infolist: [],
+            factorylist: [],
+            load_factory: null,
+            load_address: '',
+            drawerShow: false,
+            map: null,
+            dataDuble:[],
+            saveRules: {
+                product_name: [{required: true, message: '货品名称不能为空', trigger: 'blur'}],
+                product_quantity: [{required: true, message: '货品数量不能为空', trigger: 'blur'}],
+                plan_type: [{required: true, message: '请选择任务类别', trigger: 'change'}],
+                // load_factory: [{ required: true, message: '装货厂家不能为空', trigger: 'blur'}],
+                // unload_factory: [{ required: true, message: '卸货厂家不能为空',trigger: 'blur'}],
+            },
+            formData: {
+                id: 0,
+                info_id: '',
+                product_name: '',
+                product_quantity: '',
+                load_factory: '',
+                load_address: '',
+                unload_factory: '',
+                unload_address: '',
+                head_num: '',
+                trailer_num: '',
+                driver_name: '',
+                escort_name: '',
+                plan_type: '',
+                start_periodic: '',
+                end_periodic: '',
+                platform: 'pc',
+                load_location: '',
+                unload_location: ''
+            },
+        }
+    },
+    created() {
+        this.getplaninfo()
+    },
+    destroyed() {
+        if (this.map != null) {
+            this.map.destroy();
+        }
+    },
+    methods: {
+        handlePlanTypeChange(value) {
+            console.log(value)
+            if (value === 0) {
+                // 如果选择了运输任务，则设置新计费周期为必填
+                this.$set(this.saveRules, 'start_periodic', [{
+                    required: true,
+                    message: '请填写新计费周期',
+                    trigger: 'blur'
+                }]);
+            } else {
+                // 如果选择了装车任务或者卸车任务，则取消新计费周期的必填状态
+                this.$delete(this.saveRules, 'start_periodic');
+                if (this.$refs.formData) {
+                    this.$nextTick(() => {
+                        this.$refs.formData.resetFields(['start_periodic']); // 清空已填写的值
+                    });
+                }
+                this.formData.start_periodic = ''; // 清空已填写的值
+            }
+        },
+        getplaninfo() {
+            getplaninfo().then(response => {
+                if (response !== undefined) {
+                    // console.log(response.data)
+                    this.infolist = response.data
+                    this.factorylist = response.factory
+                }
+            })
+        },
+        infoChanged() {
+            const selectedinfo = this.infolist.find(item => item.id === this.formData.info_id);
+            if (selectedinfo) {
+                console.log(selectedinfo)
+                // this.formData.head_num = selectedinfo.head_num;
+                this.formData.trailer_num = selectedinfo.trailer_num;
+                this.formData.driver_name = selectedinfo.driver_name;
+                this.formData.trailer_status = selectedinfo.trailer_status;
+            } else {
+                // this.formData.head_num = '';
+                this.formData.trailer_num = '';
+                this.formData.driver_name = '';
+                this.formData.trailer_status = '';
+            }
+            // this.load_address = this.load_factory.factory;
+        },
+        loadFactoryChanged() {
+            const selectedFactory = this.factorylist.find(item => item.name === this.formData.load_factory);
+            if (selectedFactory) {
+                console.log(selectedFactory)
+                this.formData.load_address = selectedFactory.address;
+                this.formData.load_location = selectedFactory.location;
+            } else {
+                this.formData.load_address = '';
+                this.formData.load_location = '';
+            }
+            // this.load_address = this.load_factory.factory;
+        },
+        unloadFactoryChanged() {
+            const selectedFactory = this.factorylist.find(item => item.name === this.formData.unload_factory);
+            if (selectedFactory) {
+                console.log(selectedFactory)
+                this.formData.unload_address = selectedFactory.address;
+                this.formData.unload_location = selectedFactory.location;
+            } else {
+                this.formData.unload_address = '';
+                this.formData.unload_location = '';
+            }
+            // this.load_address = this.load_factory.factory;
+        },
+        handleClose() {
+            this.dialog = false
+            this.drawerShow = false
+        },
+        showForm() {
+            this.dialog = true
+            this.drawerShow = true
+            this.title = '新增任务'
+            this.resetData()
+        },
+        resetData() {
+            this.formData.id = 0
+            this.formData.info_id = ''
+            this.formData.product_name = ''
+            this.formData.product_quantity = ''
+            this.formData.load_address = ''
+            this.formData.load_factory = ''
+            this.formData.unload_address = ''
+            this.formData.unload_factory = ''
+            this.formData.head_num = ''
+            this.formData.trailer_num = ''
+            this.formData.driver_name = ''
+            this.formData.plan_type = ''
+            this.formData.start_periodic = ''
+            this.formData.end_periodic = ''
+            this.formData.load_location = ''
+            this.formData.unload_location = ''
+            this.formData.trailer_status = ''
+        },
+        getplansinfo(id) {
+            getplansinfo({id: id}).then(response => {
+                if (response !== undefined) {
+                    console.log('response.start_periodic---' + response.start_periodic)
+                    this.title = '编辑'
+                    this.formData.id = response.id
+                    this.formData.info_id = response.info_id
+                    this.formData.product_name = response.product_name
+                    this.formData.product_quantity = response.product_quantity
+                    this.formData.load_factory = response.load_factory
+                    this.formData.load_address = response.load_address
+                    this.formData.unload_address = response.unload_address
+                    this.formData.unload_factory = response.unload_factory
+                    this.formData.head_num = response.head_num
+                    this.formData.trailer_num = response.trailer_num
+                    this.formData.driver_name = response.driver_name
+                    this.formData.plan_type = response.plan_type
+                    this.formData.load_location = response.load_location
+                    this.formData.unload_location = response.unload_location
+                    this.formData.start_periodic = response.start_periodic
+                    this.formData.end_periodic = response.end_periodic
+                    this.formData.trailer_status = response.trailer_status
+                }
+            })
+        },
+        saveData() {
+            console.log(this.formData)
+            this.$confirm('您确定要提交吗？', '温馨提示')
+                .then(_ => {
+                    this.$refs.saveForm.validate(valid => {
+                        if (valid) {
+                            if (this.formData.id) {
+                                editplan(this.formData).then(_ => {
+                                    this.$message({
+                                        message: '编辑成功',
+                                        type: 'success',
+                                        duration: 5 * 1000
+                                    })
+                                    this.$emit('updateRow')
+                                    this.dialog = false
+                                })
+                            } else {
+                                //修改 No.10 start
+                                getPlanCount(this.formData).then(response => {
+                                    console.log(response)
+                                    console.log(response[0])
+                                    if(response[0] >= 2){
+                                        this.$message({
+                                            message: '已经存在两条一样的运输计划',
+                                            type: 'warning',
+                                            duration: 5 * 1000
+                                        })
+                                    }else{
+                                        addplan(this.formData).then(_ => {
+                                            this.$message({
+                                                message: '新增成功',
+                                                type: 'success',
+                                                duration: 5 * 1000
+                                            })
+                                            this.$emit('updateRow')
+                                            this.dialog = false
+                                        })
+                                    }
+                                })
+                                //修改 No.10 end
+                                //   addplan(this.formData).then(_ => {
+                                //     this.$message({
+                                //       message: '新增成功',
+                                //       type: 'success',
+                                //       duration: 5 * 1000
+                                //     })
+                                //     this.$emit('updateRow')
+                                //     this.dialog = false
+                                //   })
+                                // }
+
+                            }
+                        }
+                    })
+                })
+                .catch(_ => {
+                })
+
+        }
+    }
 }
 </script>
 
